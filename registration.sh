4f7e3b608238de68a3d81f0ebb9d132c3efecf2a
mahendrakundare <mr.kundare@gmail.com>
2019-11-21 16:46:02 +0530

    password field updated in which password should have atleast one number

--- registration.sh
+++ registration.sh
@@ -64,7 +64,7 @@ function checkPassword2(){
 #	pp="^[a-z+[[:upper:]]{1,}]{8,}$"
 #	pp="^[a-z]{7}[A-Z]{1}"	
 #	pp="^[a-zA-Z0-9]{8,}[A-Z]+[a-z0-9]$"
-	pp="^[a-z0-9]*[A-Z]+[a-zA-Z0-9]*[a-zA-Z0-9]*$"
+	pp="[a-z0-9]*[A-Z]+[a-zA-Z0-9]*$"
 	if [[ $password =~ $pp ]]
 	then
 		echo "valid password2"
@@ -73,8 +73,19 @@ function checkPassword2(){
 	fi
 }
 
+function checkPassword3(){
+	passwordPattern="[a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*[0-9]$"
+	if [[ $password =~ $passwordPattern ]]
+	then
+		echo "valid password"
+	else
+		echo "invalid password"
+	fi
+}
+
 #checkFirstName
 #checkLastName
 #checkEmail
 #checkMobile
-checkPassword2
+#checkPassword2
+checkPassword3
