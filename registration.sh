00c9f86fa8c3e7de8b858e91fbbf4b977306edd1
mahendrakundare <mr.kundare@gmail.com>
2019-11-21 10:35:32 +0530

    function added for validating user's first name

--- registration.sh
+++ registration.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -x
-echo "UserRegistration"
+shopt -s extglob #turn on extended globbing
+echo "Welcome UserRegistration"
+echo "Enter First Name"
+read firstName
+
+function checkFirstName(){
+	firstNamePattern="^[A-Z][a-zA-Z]{2,}"
+	if [[ $firstName =~ $firstNamePattern ]]
+	then
+		echo "valid first Name"
+	else
+		echo "invalid first Name"
+	fi
+}
+
+checkFirstName
